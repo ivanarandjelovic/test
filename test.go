584cb2ec3d42db2e0b14601c69c60f45b225c1b3
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2015-01-15 13:51:07 +0100

    Added gitignore, more code changes

--- test.go
+++ test.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
 
+type Man struct {
+	Name     string
+	LastName string
+	Alive    bool
+}
+
+type Configuration struct {
+	Users  []string
+	Groups []string
+	People []Man
+}
+
+func loadConfiguration() (configuration Configuration, err error) {
+	file, _ := os.Open("test_conf.json")
+	decoder := json.NewDecoder(file)
+	configuration = Configuration{}
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	return
+}
+
 func main() {
-	fmt.Println("test v2.0")
+	fmt.Println("Test v3.0")
+
+	configuration, _ := loadConfiguration()
+
+	fmt.Println(configuration.Users) // output: [UserA, UserB]
+	fmt.Println(configuration.People)
+
 }
